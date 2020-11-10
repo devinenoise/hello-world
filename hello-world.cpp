619fbe63174f7c80e64b334cc4120335e6e02640
Kyle Devine <devinenoise@gmail.com>
2020-11-09 19:41:41 -0800

    some slight refactoring

--- hello-world.cpp
+++ hello-world.cpp
@@ -5,7 +5,7 @@
 #include <string>
 using namespace std;
 
-// defining the number array
+
 int numGroup[5] = { 1, 3, 5, 7, 9 };
 
 //declare helloWorld function
@@ -18,8 +18,7 @@ int main() {
 
    cout << helloWorld(); 
 
-   
-   
+     
    numberArray();
 
    // pause the string so the terminal window remains open
@@ -27,12 +26,13 @@ int main() {
     
 }
 
+
+//define helloWorld()
 string helloWorld() {
     return "Hello World!\n";
 }
 
-// defines function to loop through numGroup
-
+// define numberArray() to loop through numGroup
 void numberArray() {
     for (int i = 0; i < 5; i++) {
         cout << numGroup[i] << endl;
