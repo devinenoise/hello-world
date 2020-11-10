2c539d2045ece2ed6ae5894b0c6956543a84daf1
Kyle Devine <devinenoise@gmail.com>
2020-11-09 19:31:39 -0800

    numberArray made into a function with comments

--- hello-world.cpp
+++ hello-world.cpp
@@ -11,19 +11,24 @@ string helloWorld() {
     return "Hello World!\n"; 
 }
 
+// defining the number array
 int numGroup[5] = { 1, 3, 5, 7, 9 };
 
+// defines function to loop through numGroup
+void numberArray() {
+    for (int i = 0; i < 5; i++) {
+        cout << numGroup[i] << endl;
+    }
+}
 
 int main() {
 
    cout << helloWorld() << endl;
 
-   for (int i = 0; i < 5; i++) {
-       cout << numGroup[i] << endl;
-   }
+   numberArray();
 
-    // pause the string so the terminal window remains open
-    system("pause>0");
+   // pause the string so the terminal window remains open
+   system("pause>0");
     
 }
 
