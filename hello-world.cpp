2ce66695c90153eb10fe2553e9d043e8278d907c
Kyle Devine <devinenoise@gmail.com>
2020-11-12 12:39:48 -0800

    using a pointer and return an array but cout displays 0

--- hello-world.cpp
+++ hello-world.cpp
@@ -3,28 +3,31 @@
 using namespace std;
 
 
-
-
-string helloWorld() {
-    return "Hello World!\n";
-}
-
-
-void numberArray() {
-    int numGroup[5] = { 1, 3, 5, 7, 9 };
-    int length = sizeof(numGroup) / sizeof(numGroup[0]);
-    for (int i = 0; i < length; i++) {
-        cout << numGroup[i] << endl;
+class HelloWorldClass {
+public:
+    string helloWorld() {
+        return "Hello World!\n";
     }
-}
+    int* numberArray() {
+        int numGroup[5] = { 1, 3, 5, 7, 9 };
+        int length = sizeof(numGroup) / sizeof(numGroup[0]);
+        for (int i = 0; i < length; i++) {
+            return numGroup;
+            cout << numGroup[i] << endl;
+        }
+    }
+};
+
 
 
 
 int main() {
 
-   cout << helloWorld(); 
+   HelloWorldClass HelloWorld;
 
-   numberArray();
+   cout << HelloWorld.helloWorld(); 
+
+   HelloWorld.numberArray(&numGroup);
 
 
 }
